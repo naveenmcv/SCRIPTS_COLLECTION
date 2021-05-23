8e10653271bb44b7d75c5d6ed8459def1b1bd559
Naveen M <83032821+naveenmcv@users.noreply.github.com>
2021-05-23 22:34:11 +0530

    Add files via upload

---
+++ UNITY_CODES_NN/PREFAB/Instantiate_destroy.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Instantiate_destroy : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public GameObject obj1;
+
+    public void On_clicknn1()
+    {
+
+        Instantiate(obj1, transform.position, transform.rotation);
+        
+    }
+
+    public void destroyobj()
+    {
+        GameObject obj_to_destroy = GameObject.Find("Cube_aa(Clone)");
+
+        Destroy(obj_to_destroy);
+    }
+
+
+    public void destroy_and_instantiate()
+    {
+        if (GameObject.Find("Cube_aa(Clone)") != null)
+        {
+            Debug.Log("found clone object");
+
+            On_clicknn1();
+            destroyobj();
+
+            //it exists
+        }
+
+        else {
+            Debug.Log("not found any clone");
+        }
+       
+
+
+    }
+
+
+
+
+
+}
